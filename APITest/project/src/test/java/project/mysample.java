31d6d5a4ca38c65187d7b6020fce957456e1e755
Prince Soni <prince.git.bucket@gmail.com>
2018-11-19 01:51:45 +0400

    version 1.0

--- APITest/project/src/test/java/project/mysample.java
+++ APITest/project/src/test/java/project/mysample.java
@@ -1,52 +1,48 @@
 package project;
 
+
 import static com.jayway.restassured.RestAssured.given;
-import static org.junit.Assert.*;
+import com.jayway.restassured.path.json.JsonPath;
+import com.jayway.restassured.response.Response;
+import com.jayway.restassured.response.ResponseBody;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import jdk.nashorn.internal.parser.JSONParser;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+import org.junit.Test;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import org.junit.Test;
-
-import com.jayway.restassured.path.json.JsonPath;
-import com.jayway.restassured.response.Response;
-import com.jayway.restassured.response.ResponseBody;
-import io.restassured.module.jsv.JsonSchemaValidator;
+import static com.jayway.restassured.RestAssured.given;
 public class mysample {
 
 	@Test
 	public void test() throws FileNotFoundException, IOException, ParseException {
-		//First URL Test
-//		ResponseBody body = given().when().get("https://api.github.com/users/dubizzle").getBody();
-//		System.out.println(body.asString());
-//		JsonPath jsonPath = new JsonPath(body.asString());
-//		String data = jsonPath.get("login");
-//		System.out.println(data);
+		//First URL Test - Validate the response
+		ResponseBody body = given().when().get("https://api.github.com/users/dubizzle").getBody();
+		System.out.println(body.asString());
+		JsonPath jsonPath = new JsonPath(body.asString());
+		String data = jsonPath.get("login");
+		System.out.println(data);
 		
-		//Second URL Test
-//		Response body = given().when().get("https://api.github.com/users/dubizzle/repos");
-////		System.out.println(body.asString());
-////		String[]   jsonPath = body.jsonPath().get();
-////		String data = jsonPath.get("login");
-//		System.out.println(body.jsonPath().get("id[1]").toString());
+		//Second URL Test - Validate the response
+		Response body1 = given().when().get("https://api.github.com/users/dubizzle/repos");
+		System.out.println(body1.jsonPath().get());
+		System.out.println(body1.jsonPath().get("id[1]").toString());
 		
-		//schema1
-//		JSONParser parser = new JSONParser();
-//		Object obj = parser.parse(new FileReader("/Users/meherchandan/eclipse-workspace/project/src/test/java/project/myschema.json"));
-////		\\Users\\meherchandan\\eclipse-workspace\\project\\src\\test\\java\\project\\myschema.json"
-//        JSONObject jsonObject = (JSONObject) obj;
-//		 given().when().get("https://api.github.com/users/dubizzle").then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonObject.toJSONString()));
+		//schema1 validation
+		JSONParser parser = new JSONParser();
+		Object obj = parser.parse(new FileReader("project/myschema.json"));
+        JSONObject jsonObject = (JSONObject) obj;
+        given().when().get("https://api.github.com/users/dubizzle").then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonObject.toJSONString()));
 
-			//schema2
-			JSONParser parser = new JSONParser();
-			Object obj = parser.parse(new FileReader("/Users/meherchandan/eclipse-workspace/project/src/test/java/project/myscema2.json"));
-//			\\Users\\meherchandan\\eclipse-workspace\\project\\src\\test\\java\\project\\myschema.json"
-	        JSONObject jsonObject = (JSONObject) obj;
-			 given().when().get("https://api.github.com/users/dubizzle/repos").then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonObject.toJSONString()));
+		 //schema2 validation
+		JSONParser parser1 = new JSONParser();
+		Object obj1 = parser1.parse(new FileReader("/project/myscema2.json"));
+		JSONObject jsonObject1 = (JSONObject) obj1;
+		given().when().get("https://api.github.com/users/dubizzle/repos").then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonObject1.toJSONString()));
 
 	}
 }
